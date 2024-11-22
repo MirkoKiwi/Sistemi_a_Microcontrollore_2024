4704b76ce1a6838f920e40e06620a3205abcd991
MirkoKiwi <106871665+MirkoKiwi@users.noreply.github.com>
2024-11-22 13:52:04 +0100

    Update polling.c

--- assignments/lab_01/src/polling.c
+++ assignments/lab_01/src/polling.c
@@ -1,11 +1,11 @@
-///*
+/*
 *  axi_gpio_1 0x40010000 
 *  axi_intc_0 0x41200000 
 *  axi_gpio_0 0x40000000 
 *  axi_uartlite_0 0x40600000 
 *  microblaze_0_local_memory_dlmb_bram_if_cntlr 0x00000000
 *  axi_gpio_2 0x40020000 
-//*/
+*/
 
 #include <stdio.h>
 #include "platform.h"
