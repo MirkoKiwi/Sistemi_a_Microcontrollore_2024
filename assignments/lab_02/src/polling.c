a1eedf5fb1cb00a75c29859f2abf6ddf11eaf40a
MirkoKiwi <106871665+MirkoKiwi@users.noreply.github.com>
2024-11-22 13:54:18 +0100

    Create polling.c

---
+++ assignments/lab_02/src/polling.c
@@ -0,0 +1,25 @@
+// Loopback
+
+#include <stdio.h>
+#include "platform.h"
+#include "xil_printf.h"
+#include "xil_io.h"
+#include "xuartlite_l.h"
+
+#define AXI_UARTLITE_0_BASEADDR 0x40600000
+
+
+int main(){
+	init_platform();
+    
+	while(1) {
+        // Riceve dati dalla periferica UARTLite
+		char ch = XUartLite_RecvByte(axi_uartlite_0);
+		
+        // Rispedisce indietro gli stessi dati ( creando una loopback )
+        XUartLite_SendByte(axi_uartlite_0, ch);
+	}
+
+    cleanup_platform();
+    return 0;
+}
