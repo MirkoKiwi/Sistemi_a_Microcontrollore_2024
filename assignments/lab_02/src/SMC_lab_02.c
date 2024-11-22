69ee2dea925f2274677bfdbb45cda5832054265a
MirkoKiwi <106871665+MirkoKiwi@users.noreply.github.com>
2024-11-22 14:51:12 +0100

    Update SMC_lab_02.c

--- assignments/lab_02/src/SMC_lab_02.c
+++ assignments/lab_02/src/SMC_lab_02.c
@@ -13,11 +13,11 @@ int main(){
 	init_platform();
     
 	while(1) {
-        // Riceve dati dalla periferica UARTLite
+        	// Riceve dati dalla periferica UARTLite
 		char ch = XUartLite_RecvByte(axi_uartlite_0);
 		
-        // Rispedisce indietro gli stessi dati ( creando una loopback )
-        XUartLite_SendByte(axi_uartlite_0, ch);
+        	// Rispedisce indietro gli stessi dati ( creando una loopback )
+        	XUartLite_SendByte(axi_uartlite_0, ch);
 	}
 
     cleanup_platform();
@@ -55,16 +55,15 @@ int main(){
 	while(1){
 		char ch = XUartLite_RecvByte(AXI_UARTLITE_0_BASEADDR);
 
-        // Se il carattere in ricezione è 'a', accende tutti i led
+        	// Se il carattere in ricezione è 'a', accende tutti i led
 		if ( ch == 'a' ) {
-            *(int *)AXI_GPIO_LEDS_BASEADDR = 0xFFFF; 
-        }
+            		*(int *)AXI_GPIO_LEDS_BASEADDR = 0xFFFF; 
+        	}
 		else {
-            // Il carattere viene convertito in numero ed i led vengono accesi secondo il suo valore
+            		// Il carattere viene convertito in numero ed i led vengono accesi secondo il suo valore
 			int num = charNumToInt(ch);
 			if ( num != -1 )    
-                *(int*)AXI_GPIO_LEDS_BASEADDR = num;
-
+                	*(int*)AXI_GPIO_LEDS_BASEADDR = num;
 		}
 	}
 
@@ -74,7 +73,7 @@ int main(){
 
 
 int charNumToInt(char ch) {
-    // Converte numero di tipo 'char' nel suo equivalente di tipo 'int', ritorna -1 se il carattere non è un numero
+    	// Converte numero di tipo 'char' nel suo equivalente di tipo 'int', ritorna -1 se il carattere non è un numero
 	return ( ch < 48 || ch > 57 ) ? -1 : ch - '0';
 }
 
@@ -113,98 +112,97 @@ u32 my_XUartLite_RecvByte(UINTPTR BaseAddress);
 void update_leds(u32 data, u8 mode);
 
 
-int main()
-{
-init_platform();
+int main() {
+	init_platform();
 
-// Registri di input
-u32 uart = 0;
-u32 buttons = 0;
-u32 switches = 0;
-
-while(1) {
-    // Aggiornamento leds in lettura da buttons
-    buttons = Xil_In32(AXI_GPIO_BUTTONS_BASEADDR);
-    update_leds(buttons, BUTTONS);
-
-    // Aggiornamento leds in lettura dagli switch
-    switches = Xil_In32(AXI_GPIO_SWITCHES_BASEADDR);
-    update_leds(switches, SWITCHES);
-
-    // Aggiornamento leds in lettura da non-blocking UART
-    uart = my_XUartLite_RecvByte(AXI_UARTLITE_0_BASEADDR);
-    update_leds(uart, UART);
-}
-
-
-cleanup_platform();
-return 0;
+	// Registri di input
+	u32 uart = 0;
+	u32 buttons = 0;
+	u32 switches = 0;
+	
+	while(1) {
+	    // Aggiornamento leds in lettura da buttons
+	    buttons = Xil_In32(AXI_GPIO_BUTTONS_BASEADDR);
+	    update_leds(buttons, BUTTONS);
+	
+	    // Aggiornamento leds in lettura dagli switch
+	    switches = Xil_In32(AXI_GPIO_SWITCHES_BASEADDR);
+	    update_leds(switches, SWITCHES);
+	
+	    // Aggiornamento leds in lettura da non-blocking UART
+	    uart = my_XUartLite_RecvByte(AXI_UARTLITE_0_BASEADDR);
+	    update_leds(uart, UART);
+	}
+	
+	
+	cleanup_platform();
+	return 0;
 }
 
 
 
 int charNumToInt(char ch) {
-// Converte numero di tipo 'char' nel suo equivalente di tipo 'int', ritorna -1 se il carattere non è un numero
-return ( ch < 48 || ch > 57 ) ? -1 : ch - '0';
+	// Converte numero di tipo 'char' nel suo equivalente di tipo 'int', ritorna -1 se il carattere non è un numero
+	return ( ch < 48 || ch > 57 ) ? -1 : ch - '0';
 }
 
 
 u32 my_XUartLite_RecvByte(UINTPTR BaseAddress) { 
-// Ritorna END OF TRANSMISSION (EOT) se il bit 0 dello status register = 1 (RX FIFO empty)
-if (XUartLite_IsReceiveEmpty(BaseAddress)) {
-    return 4; // End of Transmission (EOT) marker
-}
+	// Ritorna END OF TRANSMISSION (EOT) se il bit 0 dello status register = 1 (RX FIFO empty)
+	if (XUartLite_IsReceiveEmpty(BaseAddress)) {
+	    return 4; // End of Transmission (EOT) marker
+	}
 
-return (u8)XUartLite_ReadReg(BaseAddress, XUL_RX_FIFO_OFFSET);
+	return (u8)XUartLite_ReadReg(BaseAddress, XUL_RX_FIFO_OFFSET);
 }
 
 
 
 void update_leds(u32 data, u8 mode) {
 
-u32 currentLedStatus = *(int*)AXI_GPIO_LEDS_BASEADDR; // Lettura stato dei led
-u32 ledMask          = currentLedStatus;	
-u32 newLedOutput     = 0;
-
-char receivedChar = (u8)data; // Prendo gli 8 bit meno significativi del dato in ricezione ( di tipo char )
-
-switch (mode) {
-    case UART:
-	if ( receivedChar != 4 ) { // c = 4 implica che la UART sia vuota
-	    // Process Data
-	    if ( receivedChar == 'a' )
-		newLedOutput = 0xFF00; // Accendo i led
-	    else {
-		int number = charNumToInt(receivedChar);
-
-		if( number != -1 ) { 
-		    // Controllo i primi 8 led se il numero è valido
-		    newLedOutput = ( number << 8 ); 
+	u32 currentLedStatus = *(int*)AXI_GPIO_LEDS_BASEADDR; // Lettura stato dei led
+	u32 ledMask          = currentLedStatus;	
+	u32 newLedOutput     = 0;
+	
+	char receivedChar = (u8)data; // Prendo gli 8 bit meno significativi del dato in ricezione ( di tipo char )
+	
+	switch (mode) {
+	    case UART:
+		if ( receivedChar != 4 ) { // c = 4 implica che la UART sia vuota
+		    // Process Data
+		    if ( receivedChar == 'a' )
+			newLedOutput = 0xFF00; // Accendo i led
+		    else {
+			int number = charNumToInt(receivedChar);
+	
+			if ( number != -1 ) { 
+			    // Controllo i primi 8 led se il numero è valido
+			    newLedOutput = ( number << 8 ); 
+			}
+			else
+			    // Output invariato
+			    newLedOutput = currentLedStatus; 
+		    }
+		    // Gli ultimi 8 led rimangono invariati
+		    ledMask &= 0x00FF; 
 		}
-		else
-		    // Output invariato
-		    newLedOutput = currentLedStatus; 
-	    }
-	    // Gli ultimi 8 led rimangono invariati
-	    ledMask &= 0x00FF; 
+		break;
+	
+	    case SWITCHES:
+		// Primi 12 led invariati
+		ledMask &= 0xFFF0; 
+		newLedOutput = ( ~(data) & 0xF ); // Accendo solo gli ultimi 4 switch
+		break;
+	
+	    case BUTTONS:
+		ledMask &= 0xFF0F;  // Primi 12 led e ultimi 4 led invariati
+		newLedOutput = ( (data & 0xF) << 4 ); // Accendo solo i rimanenti 4 led in mezzo
+		break;
+	
+	    default:
+		return;
 	}
-	break;
-
-    case SWITCHES:
-	// Primi 12 led invariati
-	ledMask &= 0xFFF0; 
-	newLedOutput = ( ~(data) & 0xF ); // Accendo solo gli ultimi 4 switch
-	break;
-
-    case BUTTONS:
-	ledMask &= 0xFF0F;  // Primi 12 led e ultimi 4 led invariati
-	newLedOutput = ( (data & 0xF) << 4 ); // Accendo solo i rimanenti 4 led in mezzo
-	break;
-
-    default:
-	return;
-}
-
-// Accendo i led tenendo conto dello stato precedente
-*( (int*)AXI_GPIO_LEDS_BASEADDR ) = ledMask | newLedOutput;
+	
+	// Accendo i led tenendo conto dello stato precedente
+	*( (int*)AXI_GPIO_LEDS_BASEADDR ) = ledMask | newLedOutput;
 }
