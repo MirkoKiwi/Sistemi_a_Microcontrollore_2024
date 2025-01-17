999077470d6bdacc2f024164a143c1aa8f924c47
MirkoKiwi <106871665+MirkoKiwi@users.noreply.github.com>
2025-01-17 13:25:06 +0100

    Update blinkerstemp.c

--- assignments/lab_06/src/blinkerstemp.c
+++ assignments/lab_06/src/blinkerstemp.c
@@ -3,217 +3,322 @@
 #include "xparameters.h"
 #include "xtmrctr_l.h"
 
-// BUTTON MASKS
-#define BUTTON_MASK_LEFT  0b00010
-#define BUTTON_MASK_RIGHT 0b01000
-#define BUTTON_MASK_HAZARD 0b10000
 
-// LED MASKS
+
+// MASKS
+#define BUTTON_MASK_0 0b00001
+#define BUTTON_MASK_1 0b00010
+#define BUTTON_MASK_2 0b00100
+#define BUTTON_MASK_3 0b01000
+#define BUTTON_MASK_4 0b10000
+
+#define U32_MOST_SIGNIFICANT_BIT 0x80000000
+
+// Interrupt Controller Registers
+#define IAR 0x0C 	// Interrupt Acknowledge Register
+#define IER 0x08 	// Interrupt Enable Register
+#define MER 0x1C	// Master Enable Register
+
+// LEDS
 #define LEFT_BLINKER_MASK   0xF800
 #define RIGHT_BLINKER_MASK  0x1F
-#define HAZARD_MASK (LEFT_BLINKER_MASK | RIGHT_BLINKER_MASK)
 
-// TIMER
+// TIMERS
+#define TmrCtrNumber        0 // Timer 0
 #define TIMER_CTRL_RESET    0x56
+#define CNT_UDT0_MASK       0x00000001
 #define TIMER_T0INT_MASK    0x100
 #define TIMER_INT_SRC       0b0100
-#define TmrCtrNumber        0
 
-// INTERRUPT CONTROLLER
-#define IAR 0x0C // Interrupt Acknowledge Register
-#define IER 0x08 // Interrupt Enable Register
-#define MER 0x1C // Master Enable Register
-
-// REGISTRI
+// Registri
 volatile int *buttonsReg =  (int *)XPAR_AXI_BUTTONS_GPIO_BASEADDR;
 volatile int *leds16Reg =   (int *)XPAR_AXI_16LEDS_GPIO_BASEADDR;
 
-// GLOBAL VARIABLES
-volatile int isrSignal = 0;
+// Variabili Globali
+voltatile int isrSignal = 0;
 int timerCounter = 6000000;
-int activeFlag = 0;
+int continueFlag = 0;
 int resetFlag = 1;
+int returnIdle = 0;
 
-// ENUMERATIONS
+// Type defs 
 typedef enum { PRESSED, IDLE } debounceState_t;
 typedef enum { 
-    B_IDLE, B_LEFT_ON, B_LEFT_OFF, 
-    B_RIGHT_ON, B_RIGHT_OFF, 
-    B_HAZARD_ON, B_HAZARD_OFF
+    B_IDLE, B_L_ON, B_L_OFF, 
+    B_R_ON, B_R_OFF, 
+    B_H_ON, B_H_OFF
 } blinkersState_t;
 
-// FUNCTION DECLARATIONS
+// Funzioni Macchina a Stati
+int fsmDebounce(int buttons);
+void fsmBlinkers(int buttonsNb);
+
+// Funzioni Timer
 void init_timer(int valueCounter);
+void timer0IntAck(void);
 void timerResetCounter(int value);
 void timerEnable(void);
-void timer0IntAck(void);
-int fsmDebounce(int buttons);
-void fsmBlinkers(int buttons);
+
+// Interrupt Service Routine 
 void blinkISR(void) __attribute__((interrupt_handler));
 
-// MAIN FUNCTION
+
+
 int main() {
     init_platform();
+
     microblaze_enable_interrupts();
 
+    //
     *leds16Reg = 0x00;
+    int buttonsNb = 0;
+
     init_timer(timerCounter);
 
     while (1) {
-        int buttons = fsmDebounce(*buttonsReg);
-        fsmBlinkers(buttons);
+        buttonsNb = fsmDebounce(*buttonsReg);
+        fsmBlinkers(buttonsNb);
     }
 
     cleanup_platform();
     return 0;
 }
 
-// DEBOUNCE FSM
-int fsmDebounce(int buttons) {
-    static debounceState_t state = IDLE;
-    static int debouncedButtons = 0;
 
-    switch (state) {
+
+// Funzioni Macchina a Stati
+int fsmDebounce(int buttons) {
+    static int debouncedButtons;
+    static debounceState_t currentState = IDLE;
+
+    switch (currentState) {
         case IDLE:
-            if (buttons != 0) {
-                state = PRESSED;
-                debouncedButtons = buttons;
+            debouncedButtons = buttons;
+            if ( buttons != 0 ) {
+                currentState = PRESSED;
             }
             break;
+
         case PRESSED:
-            if (buttons == 0) {
-                state = IDLE;
-                debouncedButtons = 0;
+            debouncedButtons = 0;
+            if ( buttons == 0 ) {
+                currentState = IDLE;
             }
             break;
     }
+
     return debouncedButtons;
 }
 
-// BLINKER FSM
-void fsmBlinkers(int buttons) {
-    static blinkersState_t state = B_IDLE;
-    static int ledOutput = 0;
+void fsmBlinkers(int buttonsNb) {
+    static u32 ledOutput;
+    static blinkersState_t currentState = B_IDLE;
 
-    int leftPressed = buttons & BUTTON_MASK_LEFT;
-    int rightPressed = buttons & BUTTON_MASK_RIGHT;
-    int hazardPressed = buttons & BUTTON_MASK_HAZARD;
+    int leftButton = buttonsNb & BUTTON_MASK_1;
+    int rightButton = buttonsNb & BUTTON_MASK_3;
+    int hazardButton = buttonsNb & BUTTON_MASK_4;
 
-    switch (state) {
+    switch (currentState) {
+        // Idle
         case B_IDLE:
             timerResetCounter(timerCounter);
+            continueFlag = 0;
             ledOutput = 0;
             resetFlag = 1;
-            activeFlag = 0;
+            returnIdle = 0;
 
-            if (leftPressed) {
-                state = B_LEFT_ON;
+            if ( leftButton ) {
                 timerEnable();
                 resetFlag = 0;
-                activeFlag = 1;
-            } else if (rightPressed) {
-                state = B_RIGHT_ON;
+                continueFlag = 1;
+                currentState = B_L_ON;
+            } 
+            else if ( rightButton ) {
                 timerEnable();
                 resetFlag = 0;
-                activeFlag = 1;
-            } else if (hazardPressed) {
-                state = B_HAZARD_ON;
+                continueFlag = 1;
+                currentState = B_R_ON;
+            } 
+            else if ( hazardButton ) {
                 timerEnable();
                 resetFlag = 0;
-                activeFlag = 1;
+                continueFlag = 1;
+                currentState = B_H_ON;
             }
             break;
 
-        case B_LEFT_ON:
-            ledOutput |= LEFT_BLINKER_MASK >> isrSignal;
-            if (isrSignal > 4 || !leftPressed) {
-                state = leftPressed ? B_IDLE : B_LEFT_OFF;
+        // Left
+        case B_L_ON:
+            continueFlag = 1;
+
+            if ( leftButton ) {
+                returnIdle = 1;
+            }
+
+            if ( !leftButton && isrSignal > 5 ) {
+                currentState = B_L_OFF;
                 resetFlag = 1;
+                continueFlag = 0;
+
+                while (isrSignal);
+                
+                resetFlag = 0;
             }
+
+            ledOutput |= ( 1 << isrSignal ) << 10;
             break;
 
-        case B_LEFT_OFF:
+        case B_L_OFF:
+            continueFlag = 1;
             ledOutput = 0;
-            if (!leftPressed) {
-                state = B_LEFT_ON;
+
+            if ( leftButton || returnIdle ) {
+                currentState = B_IDLE;
+            } 
+            else if ( !leftButton && isrSignal > 2 ) {
+                currentState = B_L_ON;
                 resetFlag = 1;
-            } else {
-                state = B_IDLE;
+                continueFlag = 0;
+                
+                while (isrSignal);
+                
+                resetFlag = 0;
             }
             break;
 
-        case B_RIGHT_ON:
-            ledOutput |= RIGHT_BLINKER_MASK << isrSignal;
-            if (isrSignal > 4 || !rightPressed) {
-                state = rightPressed ? B_IDLE : B_RIGHT_OFF;
-                resetFlag = 1;
+        // Right
+        case B_R_ON:
+            continueFlag = 1;
+
+            if ( rightButton ) {
+                returnIdle = 1;
             }
+
+            if ( !rightButton && isrSignal > 5 ) {
+                currentState = B_R_OFF;
+                resetFlag = 1;
+                continueFlag = 0;
+                
+                while (isrSignal);
+                
+                resetFlag = 0;
+            }
+
+            ledOutput |= ( U32_MOST_SIGNIFICANT_BIT >> isrSignal ) >> 26;
             break;
 
-        case B_RIGHT_OFF:
+        case B_R_OFF:
+            continueFlag = 1;
             ledOutput = 0;
-            if (!rightPressed) {
-                state = B_RIGHT_ON;
+
+            if ( rightButton || returnIdle ) {
+                currentState = B_IDLE;
+            } 
+            else if ( !rightButton && isrSignal > 2 ) {
+                currentState = B_R_ON;
                 resetFlag = 1;
-            } else {
-                state = B_IDLE;
+                continueFlag = 0;
+                
+                while (isrSignal);
+                
+                resetFlag = 0;
             }
             break;
 
-        case B_HAZARD_ON:
-            ledOutput |= HAZARD_MASK;
-            if (isrSignal > 4 || !hazardPressed) {
-                state = hazardPressed ? B_IDLE : B_HAZARD_OFF;
-                resetFlag = 1;
+        // Hazard
+        case B_H_ON:
+            continueFlag = 1;
+
+            if ( hazardButton ) {
+                returnIdle = 1;
             }
+
+            if ( !hazardButton && isrSignal > 5 ) {
+                currentState = B_H_OFF;
+                resetFlag = 1;
+                continueFlag = 0;
+                
+                while (isrSignal);
+                
+                resetFlag = 0;
+            }
+
+            ledOutput |= ( ( 1 << isrSignal ) << 10 ) | ( ( U32_MOST_SIGNIFICANT_BIT >> isrSignal ) >> 26 );
             break;
 
-        case B_HAZARD_OFF:
+        case B_H_OFF:
+            continueFlag = 1;
             ledOutput = 0;
-            if (!hazardPressed) {
-                state = B_HAZARD_ON;
+
+            if ( hazardButton || returnIdle ) {
+                currentState = B_IDLE;
+            } 
+            else if ( !hazardButton && isrSignal > 2 ) {
+                currentState = B_H_ON;
                 resetFlag = 1;
-            } else {
-                state = B_IDLE;
+                continueFlag = 0;
+                
+                while (isrSignal);
+                
+                resetFlag = 0;
             }
             break;
     }
+
     *leds16Reg = ledOutput;
 }
 
-// TIMER FUNCTIONS
 void init_timer(int counterValue) {
-    *(int *)(XPAR_AXI_INTC_0_BASEADDR + MER) = 0x3;  // Enable MER
-    *(int *)(XPAR_AXI_INTC_0_BASEADDR + IER) = TIMER_INT_SRC; // Enable timer interrupt
+    // Abilita interrupt
+    *(int *)(XPAR_AXI_INTC_0_BASEADDR + MER) = 0b11;  // Abilita MER
+    *(int *)(XPAR_AXI_INTC_0_BASEADDR + IER) = 0b110; // Abilita IER
 
-    XTmrCtr_SetControlStatusReg(XPAR_AXI_TIMER_0_BASEADDR, TmrCtrNumber, TIMER_CTRL_RESET);
-    XTmrCtr_SetLoadReg(XPAR_AXI_TIMER_0_BASEADDR, TmrCtrNumber, counterValue);
-    XTmrCtr_LoadTimerCounterReg(XPAR_AXI_TIMER_0_BASEADDR, TmrCtrNumber);
+    // Configurazione Timer
+    XTmrCtr_SetControlStatusReg(XPAR_AXI_TIMER_0_BASEADDR, TmrCtrNumber, TIMER_CTRL_RESET); 	// Configura Status Register (SR)
+    XTmrCtr_SetLoadReg(XPAR_AXI_TIMER_0_BASEADDR, TmrCtrNumber, counterValue);  				// Load register impostato su counterValue
+    XTmrCtr_LoadTimerCounterReg(XPAR_AXI_TIMER_0_BASEADDR, TmrCtrNumber);       				// Inizializza il timer
 
+    // Fa partire il timer resettando il bit di LOAD0
+    u32 controlStatus = XTmrCtr_GetControlStatusReg(XPAR_AXI_TIMER_0_BASEADDR, TmrCtrNumber);
+    XTmrCtr_SetControlStatusReg(XPAR_AXI_TIMER_0_BASEADDR, TmrCtrNumber, controlStatus & ~XTC_CSR_LOAD_MASK);
+
+    // Attiva il timer
     timerEnable();
+}
+
+void timer0IntAck(void) {
+    // Acknowledge interrupt del timer
+    *(int *)XPAR_AXI_TIMER_0_BASEADDR |= TIMER_T0INT_MASK;
+
+    // Acknowledge Interrupt IAR
+    *(int *)(XPAR_AXI_INTC_0_BASEADDR + IAR) = TIMER_INT_SRC;	// Acknowledge Global Interrupt
 }
 
 void timerResetCounter(int value) {
     XTmrCtr_Disable(XPAR_AXI_TIMER_0_BASEADDR, TmrCtrNumber);
     XTmrCtr_SetLoadReg(XPAR_AXI_TIMER_0_BASEADDR, TmrCtrNumber, value);
     XTmrCtr_LoadTimerCounterReg(XPAR_AXI_TIMER_0_BASEADDR, TmrCtrNumber);
+
+    u32 controlStatus = XTmrCtr_GetControlStatusReg(XPAR_AXI_TIMER_0_BASEADDR, TmrCtrNumber);
+    XTmrCtr_SetControlStatusReg(XPAR_AXI_TIMER_0_BASEADDR, TmrCtrNumber, controlStatus & (~XTC_CSR_LOAD_MASK));
 }
 
 void timerEnable(void) {
     XTmrCtr_Enable(XPAR_AXI_TIMER_0_BASEADDR, TmrCtrNumber);
 }
 
-void timer0IntAck(void) {
-    *(int *)XPAR_AXI_TIMER_0_BASEADDR |= TIMER_T0INT_MASK;
-    *(int *)(XPAR_AXI_INTC_0_BASEADDR + IAR) = TIMER_INT_SRC;
-}
-
-// INTERRUPT SERVICE ROUTINE
+// Interrupt Service Routine 
 void blinkISR(void) {
-    if (resetFlag) {
-        isrSignal = 0;
-    } else {
-        isrSignal++;
+    int interruptSource = *(int *)XPAR_AXI_INTC_0_BASEADDR;
+
+    if ( interruptSource & TIMER_INT_SRC ) {
+        if ( resetFlag ) {
+            isrSignal = 0;
+        } 
+        else if ( continueFlag ) {
+            isrSignal++;
+        }
+
+        timer0IntAck();
     }
-    timer0IntAck();
 }
